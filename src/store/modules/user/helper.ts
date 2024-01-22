83b1694fe79c2823c4b4b609ac892a821740571e
MicroGalaxy <327288751@qq.com>
2024-01-22 21:22:33 +0800

    Revert "【修改】金豪漾展示"
    This reverts commit ee09a77c38606a750187b9b569c7112cb703246a.

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: '',
-      name: 'Hoyar',
-      description: '金豪漾',
+      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
+      name: 'ChenZhaoYu',
+      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
